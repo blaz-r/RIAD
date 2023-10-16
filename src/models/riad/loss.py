4c4d8a42c66f4adf2435fc4f883471c0003b24c2
blaz-r <blaz.rolih@gmail.com>
2023-10-16 18:57:41 +0200

    Change ssim loss

--- src/models/riad/loss.py
+++ src/models/riad/loss.py
@@ -4,15 +4,95 @@ import torch.nn.functional as F
 from torch import Tensor
 import torch.nn as nn
 from torchvision.transforms.v2 import Grayscale
+import math
 
 
-class SSIMLoss(nn.Module):
-    def __init__(self):
+class SSIMLoss(torch.nn.Module):
+    """
+    Taken from https://github.com/VitjanZ/DRAEM/blob/main/loss.py
+
+    """
+    def __init__(self, window_size=11, size_average=True, val_range=None):
         super().__init__()
-        self.ssim = StructuralSimilarityIndexMeasure()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.val_range = val_range
 
-    def forward(self, img: Tensor, img_r: Tensor) -> Tensor:
-        return 1 - self.ssim(img_r, img)
+        # Assume 1 channel for SSIM
+        self.channel = 1
+        self.window = self.create_window(window_size).cuda()
+
+    def forward(self, img1, img2):
+        (_, channel, _, _) = img1.size()
+
+        if channel == self.channel and self.window.dtype == img1.dtype:
+            window = self.window
+        else:
+            window = self.create_window(self.window_size, channel).to(img1.device).type(img1.dtype)
+            self.window = window
+            self.channel = channel
+
+        s_score, ssim_map = self.ssim(img1, img2, window=window, window_size=self.window_size, size_average=self.size_average)
+        return 1.0 - s_score
+
+    @staticmethod
+    def gaussian(window_size, sigma):
+        gauss = torch.Tensor(
+            [math.exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
+        return gauss / gauss.sum()
+
+    def create_window(self, window_size, channel=1):
+        _1D_window = self.gaussian(window_size, 1.5).unsqueeze(1)
+        _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+        window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
+        return window
+
+    def ssim(self, img1, img2, window_size=11, window=None, size_average=True, full=False, val_range=None):
+        if val_range is None:
+            if torch.max(img1) > 128:
+                max_val = 255
+            else:
+                max_val = 1
+
+            if torch.min(img1) < -0.5:
+                min_val = -1
+            else:
+                min_val = 0
+            l = max_val - min_val
+        else:
+            l = val_range
+
+        padd = window_size // 2
+        (_, channel, height, width) = img1.size()
+
+        mu1 = F.conv2d(img1, window, padding=padd, groups=channel)
+        mu2 = F.conv2d(img2, window, padding=padd, groups=channel)
+
+        mu1_sq = mu1.pow(2)
+        mu2_sq = mu2.pow(2)
+        mu1_mu2 = mu1 * mu2
+
+        sigma1_sq = F.conv2d(img1 * img1, window, padding=padd, groups=channel) - mu1_sq
+        sigma2_sq = F.conv2d(img2 * img2, window, padding=padd, groups=channel) - mu2_sq
+        sigma12 = F.conv2d(img1 * img2, window, padding=padd, groups=channel) - mu1_mu2
+
+        c1 = (0.01 * l) ** 2
+        c2 = (0.03 * l) ** 2
+
+        v1 = 2.0 * sigma12 + c2
+        v2 = sigma1_sq + sigma2_sq + c2
+        cs = torch.mean(v1 / v2)  # contrast sensitivity
+
+        ssim_map = ((2 * mu1_mu2 + c1) * v1) / ((mu1_sq + mu2_sq + c1) * v2)
+
+        if size_average:
+            ret = ssim_map.mean()
+        else:
+            ret = ssim_map.mean(1).mean(1).mean(1)
+
+        if full:
+            return ret, cs
+        return ret, ssim_map
 
 
 class MSGMS(nn.Module):
@@ -95,13 +175,13 @@ class RIADLoss(nn.Module):
 
 if __name__ == "__main__":
     from kornia.losses import ssim_loss
-    loss = SSIMLoss()
+    my_loss = SSIMLoss()
 
     x = torch.rand(4, 3, 50, 50)
     y = torch.rand(4, 3, 50, 50)
 
-    print(loss(x, y))
-    print(ssim_loss(x, y, 11) * 2)
+    print(my_loss(x, y))
+    print(ssim_loss(y, x, 11) * 2)
 
     # msgsm = MSGMS().cuda()
     # map = msgsm(x.cuda(), x.cuda(), as_map=True)
